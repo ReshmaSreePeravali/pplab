9b85e2a25e8dc92316ae649883b9bba4c89371ce
ReshmaSreePeravali <peravalireshmasreesai@gmail.com>
2026-03-07 10:35:20 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,19 @@
+
+for i in range(1,11):
+    print(i)
+
+# list using for() loop
+l=[1,2,"kits ","college"]
+for i in l:
+    print(i)
+
+# tuple using for() loop
+t=("APPLE","BANANA","ORANGE",)
+for i in t:
+    print(i)
+
+# string using for() loop
+s='kits'
+for i in s:
+    print(i)
+
---
+++ while_loop.py
@@ -0,0 +1,13 @@
+
+i=1
+
+while(i<10):
+    print(i)
+    i+=1
+
+l=[1,2,"kits","college"]
+
+i=0
+while(i<len(l)):
+    print(l[i])
+    i=i+1
